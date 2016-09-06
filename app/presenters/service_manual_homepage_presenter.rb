1cf1151c3362318a243ca0944c6c88fb8d1621ec
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-09-06 10:46:38 +0100

    Include linked topics on the service manual homepage

--- app/presenters/service_manual_homepage_presenter.rb
+++ app/presenters/service_manual_homepage_presenter.rb
@@ -2,4 +2,14 @@ class ServiceManualHomepagePresenter < ContentItemPresenter
   def breadcrumbs
     []
   end
+
+  def topics
+    unsorted_topics.sort_by { |topic| topic["title"] }
+  end
+
+private
+
+  def unsorted_topics
+    @_topics ||= links["children"] || []
+  end
 end
