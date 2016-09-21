27c1a65785528ad26626cc8d354964f708424e4b
Gemma Leigh <hello@gemmaleigh.com>
2016-09-21 16:00:01 +0100

    Add a checkResize function
    Use this to check when the screen is resized and remove the sticky nav
    if the width is below 768px.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 // from govuk_frontend_toolkit
-//= require govuk/stick-at-top-when-scrolling
+//= require stick-at-top-when-scrolling
 //= require govuk/stop-scrolling-at-footer
-
 //= require_tree .
 
 window.GOVUK.stickAtTopWhenScrolling.init();
